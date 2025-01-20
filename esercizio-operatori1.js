57a7108c67221cefe074d0a6776a3660b6c850db
Lorenzo05-creator <lory4jz@gmail.com>
2025-01-20 13:18:43 +0100

    Add files via upload

---
+++ esercizio-operatori1.js
@@ -0,0 +1,21 @@
+//SOMMA + MEDIA
+
+let num1 = 2;
+let num2 = 4;
+let num3 = 7;
+let num4 = 9;
+let num5 = 12;
+
+let somma;
+ somma = num1 + num2 + num3 + num4 + num5;
+ console.log(somma);
+
+let media;
+media = (num1 + num2 + num3 + num4 + num5) / 5;
+console.log(media);
+
+let str1 = 'la somma tra i numeri equivale a ';
+let str2 = ' e la media equivale a ';
+
+valore = str1 + somma + str2 + media;
+console.log(valore);
